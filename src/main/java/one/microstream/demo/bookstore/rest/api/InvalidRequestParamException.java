3278de654410e1919e42ed4302fa541086752a68
Benedikt Full <74531112+fullben@users.noreply.github.com>
2021-11-21 13:26:57 +0100

    Declare serialVersionUID private

--- src/main/java/one/microstream/demo/bookstore/rest/api/InvalidRequestParamException.java
+++ src/main/java/one/microstream/demo/bookstore/rest/api/InvalidRequestParamException.java
@@ -7,7 +7,7 @@ package one.microstream.demo.bookstore.rest.api;
  */
 public class InvalidRequestParamException extends RuntimeException {
 
-  static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
   private final String requestParamName;
 
   public InvalidRequestParamException(String requestParamName) {
